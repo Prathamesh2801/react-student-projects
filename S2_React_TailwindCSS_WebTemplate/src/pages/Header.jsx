57c1499afa1fafc9c843d740da023efb507a2efa
Prathamesh2801 <truepratham2801@gmail.com>
2025-11-06 11:31:04 +0530

    S2_React_TailwindCSS_WebTemplate

---
+++ S2_React_TailwindCSS_WebTemplate/src/pages/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Banners from '../components/Banners'
+import Navbar from '../components/Navbar'
+
+export default function Header() {
+  return (
+    <>
+      <Banners/>
+      <Navbar/>
+    </>
+  )
+}
